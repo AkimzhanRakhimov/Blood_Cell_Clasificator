4c5a95260a0bd1199fabeaee6343d97a98123da5
Akimzhan Rakhimov <163539376+AkimzhanRakhimov@users.noreply.github.com>
2026-03-18 13:45:52 +0500

    Create utils.py

---
+++ src/model/utils.py
@@ -0,0 +1,6 @@
+# save model
+# torch.save(obj=model.state_dict(),f=MODEL_SAVE_PATH),
+# print(f"Model saved to {MODEL_SAVE_PATH}")
+
+# load model
+# model.load_state_dict(torch.load(MODEL_SAVE_PATH))
