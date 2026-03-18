bc9a76c599e5dfd9394226ddee3337b0776ee263
Akimzhan Rakhimov <163539376+AkimzhanRakhimov@users.noreply.github.com>
2026-03-18 13:43:16 +0500

    Add BloodCellClassifierV0 neural network model
    Implemented a neural network model for blood cell classification with two convolutional blocks and a linear layer.

---
+++ src/model/model_architecture.py
@@ -0,0 +1,47 @@
+# neural network consists of 2 convolutional blocks and 1 linear layer
+class BloodCellClassifierV0(nn.Module):
+  def __init__(self,input_features:int,hidden_dim:int,output_features:int):
+    super().__init__()
+    self.conv_block_1=nn.Sequential(
+        nn.Conv2d(in_channels=input_features,
+                  out_channels=hidden_dim,
+                  kernel_size=3,
+                  padding=1,
+                  stride=1),
+        nn.BatchNorm2d(hidden_dim),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=hidden_dim,
+                  out_channels=hidden_dim,
+                  kernel_size=3,
+                  padding=1,
+                  stride=1),
+        nn.BatchNorm2d(hidden_dim),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(2))
+    )
+    self.conv_block_2=nn.Sequential(
+        nn.Conv2d(in_channels=hidden_dim,
+                  out_channels=hidden_dim,
+                  kernel_size=3,
+                  padding=1,
+                  stride=1),
+        nn.BatchNorm2d(hidden_dim),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=hidden_dim,
+                  out_channels=hidden_dim,
+                  kernel_size=3,
+                  padding=1,
+                  stride=1),
+        nn.BatchNorm2d(hidden_dim),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(2))
+    )
+    self.classifier=nn.Sequential(
+        nn.Flatten(),
+        nn.Dropout(0.5),
+        nn.Linear(in_features=hidden_dim*4800,
+                  out_features=output_features),
+    )
+
+  def forward(self,x):
+    return self.classifier(self.conv_block_2(self.conv_block_1(x)))
